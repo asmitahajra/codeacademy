607a2c48251996337cb71c1fea425a75d961fc85
asmita <asmitahajra@gmail.com>
2021-02-05 06:56:25 +0530

    updated exc till loops

---
+++ javascript/hello.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+var a=10;
+var b=5;
+var c=a+b;
+console.log(c);
---
+++ javascript/javascripting/strings.js
@@ -0,0 +1,4 @@
+const someString= 'this is a string'
+const somestring='This is Amy\’s. laptop'
+console.log(someString)
+console.log(somestring)
